c3250e9250a4edabdab5a4f8c99f60638aa44a91
Kalai <skl.rsn@gmail.com>
2023-03-11 22:10:48 +0200

    postgres protocol

--- postgres/backend.go
+++ postgres/backend.go
@@ -4,15 +4,29 @@ package postgres
  * Message Formats
  * https://www.postgresql.org/docs/current/protocol-message-formats.html
  *
- * (B)
+ * Backend (B)
  */
 
+/**
+ * SSLResponse (B)
+ *
+ * To initiate an SSL-encrypted connection, the frontend initially sends an SSLRequest message rather than a StartupMessage.
+ * The server then responds with a single byte containing S or N, indicating that it is willing or unwilling to perform SSL, respectively
+ */
 func SSLResponseMessage(sslCode byte) []byte {
 	message := NewMessageBuffer()
 	message.WriteByte(sslCode)
 	return message.Bytes()
 }
 
+/**
+ * ReadyForQuery (B)
+ *
+ * Processing of the query string is complete.
+ * A separate message is sent to indicate this because the query string might contain multiple SQL commands.
+ * (CommandComplete marks the end of processing one SQL command, not the whole string.)
+ * ReadyForQuery will always be sent, whether processing terminates successfully or with an error.
+ */
 func ReadyForQueryMessage() []byte {
 	message := NewMessageBuffer()
 	message.WriteByte(MessageTypeReadyForQuery)
@@ -21,6 +35,12 @@ func ReadyForQueryMessage() []byte {
 	return message.Bytes()
 }
 
+/**
+ * BackendKeyData (B)
+ *
+ * This message provides secret-key data that the frontend must save if it wants to be able to issue cancel requests later.
+ *  The frontend should not respond to this message, but should continue listening for a ReadyForQuery message.
+ */
 func BackendKeyDataMessage(processID, secretKey int32) []byte {
 	message := NewMessageBuffer()
 	message.WriteByte(MessageTypeBackendKeyData)
@@ -30,6 +50,13 @@ func BackendKeyDataMessage(processID, secretKey int32) []byte {
 	return message.Bytes()
 }
 
+/**
+ * ParameterStatus (B)
+ *
+ * This message informs the frontend about the current (initial) setting of backend parameters, such as client_encoding or DateStyle.
+ * The frontend can ignore this message, or record the settings for its future use;
+ * The frontend should not respond to this message, but should continue listening for a ReadyForQuery message.
+ */
 func ParameterStatusMessage(parameterName, parameterValue string) []byte {
 	message := NewMessageBuffer()
 	message.WriteByte(MessageTypeParameterStatus)
@@ -40,6 +67,11 @@ func ParameterStatusMessage(parameterName, parameterValue string) []byte {
 	return message.Bytes()
 }
 
+/**
+ * AuthenticationOk (B)
+ *
+ * This message informs the frontend about the authentication exchange is successfully completed.
+ */
 func AuthenticationOkResponseMessage() []byte {
 	message := NewMessageBuffer()
 	message.WriteByte(MessageTypeAuthentication)
@@ -48,6 +80,12 @@ func AuthenticationOkResponseMessage() []byte {
 	return message.Bytes()
 }
 
+/**
+ * AuthenticationClearTextPassword (B)
+ *
+ * The frontend must now send a PasswordMessage containing the password in clear-text form.
+ * If this is the correct password, the server responds with an AuthenticationOk, otherwise it responds with an ErrorResponse.
+ */
 func AuthenticationClearTextPasswordRequestMessage() []byte {
 	message := NewMessageBuffer()
 	message.WriteByte(MessageTypeAuthentication)
--- postgres/frontend.go
+++ postgres/frontend.go
@@ -4,9 +4,15 @@ package postgres
  * Message Formats
  * https://www.postgresql.org/docs/current/protocol-message-formats.html
  *
- * (F)
+ * Frontend (F)
  */
 
+/**
+ * PasswordMessage (F)
+ *
+ * The server then sends an appropriate authentication request message,
+ *  to which the frontend must reply with an appropriate authentication response message (such as a password)
+ */
 func CreatePasswordResponseMessage(password string) []byte {
 	message := NewMessageBuffer()
 	message.WriteByte(MessageTypePasswordResponse)
@@ -16,6 +22,13 @@ func CreatePasswordResponseMessage(password string) []byte {
 	return message.Bytes()
 }
 
+/**
+ * StartupMessage (F)
+ *
+ * To begin a session, a frontend opens a connection to the server and sends a startup message.
+ * This message includes the names of the user and of the database the user wants to connect to;
+ * it also identifies the particular protocol version to be used
+ */
 func CreateStartupMessage(username string, database string, options map[string]string) []byte {
 	message := NewMessageBuffer()
 	message.WriteInt32(0)
@@ -33,6 +46,12 @@ func CreateStartupMessage(username string, database string, options map[string]s
 	return message.Bytes()
 }
 
+/**
+ * SSLRequest (F)
+ *
+ * To initiate an SSL-encrypted connection, the frontend initially sends an SSLRequest message rather than a StartupMessage.
+ * The server then responds with a single byte containing S or N, indicating that it is willing or unwilling to perform SSL, respectively.
+ */
 func SSLRequestMessage() []byte {
 	message := NewMessageBuffer()
 	message.WriteInt32(8)
